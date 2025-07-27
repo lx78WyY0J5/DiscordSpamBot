c2ae7d1832e2be247ba5778ab824cf0f150b155c
lx78WyY0J5 <9j2k37st@gmail.com>
2025-07-27 03:23:01 +0200

    fix gitignore & line to feed commit

--- src/main/java/altherneum/fr/main/token.java
+++ src/main/java/altherneum/fr/main/token.java
@@ -4,5 +4,5 @@ public class token {
     public static String token = "AAAAAAAAAAAAAAAAAAAAA.AAAAAA.AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA";
     public static String UserID = "123456789123456789";
 
-    public static String message = "Super message that will be spammed :3 \nSecond line with 😎";
+    public static String message = "Super message that will be spammed :3 \nSecond line with 😎\nAnother line";
 }
